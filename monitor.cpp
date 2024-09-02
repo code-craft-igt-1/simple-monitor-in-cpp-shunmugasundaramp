940028d59626ad9fc552d888fb30b04f7be501f5
Ponniah <ShunmugaSundaram.P@philips.com>
2024-09-02 16:57:51 +0530

    Refining code

--- monitor.cpp
+++ monitor.cpp
@@ -3,7 +3,8 @@
 #include <thread>
 #include <chrono>
 #include <iostream>
-using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
+#include <string>
+using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds, std::string;
 
 bool CheckMargins(float minRange, float maxRange, float value) {
   bool isValueInRange = false;
@@ -13,7 +14,8 @@ bool CheckMargins(float minRange, float maxRange, float value) {
   return isValueInRange;
 }
 
-void ShowWarning() {
+void ShowWarning(string& message) {
+  cout << message << "\n";
   for (int i = 0; i < 6; i++) {
     cout << "\r* " << flush;
     sleep_for(seconds(1));
@@ -22,38 +24,38 @@ void ShowWarning() {
   }
 }
 
-bool isTemperatureCritical(float temperature) {
+bool isTemperatureCritical(float temperature, string& message) {
   bool isTemperatureCritical = false;
   if (!CheckMargins(TEMPERATURE_LOWER_LIMIT, TEMPERATURE_UPPER_LIMIT, temperature)) {
-    cout << "Temperature is critical!\n";
-    ShowWarning();
+    ShowWarning(message);
     isTemperatureCritical = true;
   }
   return isTemperatureCritical;
 }
 
-bool isPulseRateOutOfRange(float pulseRate) {
+bool isPulseRateOutOfRange(float pulseRate, string& message) {
   bool isPulseRateOutOfRange = false;
   if (!CheckMargins(PULSE_RATE_LOWER_LIMIT, PULSE_RATE_UPPER_LIMIT, pulseRate)) {
-    cout << "Pulse Rate is out of range!\n";
-    ShowWarning();
+    ShowWarning(message);
     isPulseRateOutOfRange = true;
   }
   return isPulseRateOutOfRange;
 }
 
-bool isSpo2OutOfRange(float spo2) {
+bool isSpo2OutOfRange(float spo2, string& message) {
   bool isSpo2OutOfRange = false;
   if (!CheckMargins(SPO2_LOWER_LIMIT, SPO2_UPPER_LIMIT, spo2)) {
-    cout << "Oxygen Saturation out of range!\n";
-    ShowWarning();
+    ShowWarning(message);
     isSpo2OutOfRange = true;
   }
   return isSpo2OutOfRange;
 }
 
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-  return !(isTemperatureCritical(temperature) ||
-           isPulseRateOutOfRange(pulseRate) ||
-           isSpo2OutOfRange(spo2));
+  string temperatureMessage = "Temperature is critical!";
+  string pulseRateMessage = "Pulse Rate is out of range!";
+  string spo2Message = "Oxygen Saturation out of range!";
+  return !(isTemperatureCritical(temperature, temperatureMessage) ||
+           isPulseRateOutOfRange(pulseRate, pulseRateMessage) ||
+           isSpo2OutOfRange(spo2, spo2Message));
 }
