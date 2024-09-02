e59088993413ff8d98ea8e2172139e8897f4207e
Ponniah <ShunmugaSundaram.P@philips.com>
2024-09-02 14:42:28 +0530

    Strengthen the test

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -2,10 +2,27 @@
 
 #include "gtest/gtest.h"
 
-TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-  ASSERT_FALSE(vitalsOk(99, 102, 100));
-  ASSERT_FALSE(vitalsOk(105, 75, 100));
-  ASSERT_FALSE(vitalsOk(99, 75, 70));
-  ASSERT_FALSE(vitalsOk(105, 102, 100));
+TEST(Monitor, EverythingIsOk) {
   ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+}
+TEST(Monitor, OnlyTemperatureIsOffRange) {
+  ASSERT_FALSE(vitalsOk(105, 75, 100)); 
+}
+TEST(Monitor, OnlyPulseRateIsOffRange) {
+  ASSERT_FALSE(vitalsOk(99, 102, 100));
+}
+TEST(Monitor, OnlySpo2IsOffRange) {
+  ASSERT_FALSE(vitalsOk(99, 75, 70));
+}
+TEST(Monitor, WhenTemperatureAndPulseRateAreOffRange) {
+  ASSERT_FALSE(vitalsOk(105, 102, 100));
+}
+TEST(Monitor, WhenTemperatureAndSpo2AreOffRange) {
+  ASSERT_FALSE(vitalsOk(105, 75, 70));
+}
+TEST(Monitor, WhenPulseRateAndSpo2AreOffRange) {
+  ASSERT_FALSE(vitalsOk(99, 102, 70));
+}
+TEST(Monitor, WhenAllAreInBorderLine) {
+  ASSERT_FALSE(vitalsOk(102, 100, 90));
 }
