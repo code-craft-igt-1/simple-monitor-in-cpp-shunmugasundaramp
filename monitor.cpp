2cc922cdafcd851950c0e8206c917178b4484d57
Ponniah <ShunmugaSundaram.P@philips.com>
2024-09-02 14:59:59 +0530

    Strengthen the test

--- monitor.cpp
+++ monitor.cpp
@@ -24,7 +24,7 @@ void ShowWarning() {
 
 bool isTemperatureCritical(float temperature) {
   bool isTemperatureCritical = false;
-  if (!CheckMargins(95, 102, temperature)) {
+  if (!CheckMargins(TEMPERATURE_LOWER_LIMIT, TEMPERATURE_UPPER_LIMIT, temperature)) {
     cout << "Temperature is critical!\n";
     ShowWarning();
     isTemperatureCritical = true;
@@ -34,7 +34,7 @@ bool isTemperatureCritical(float temperature) {
 
 bool isPulseRateOutOfRange(float pulseRate) {
   bool isPulseRateOutOfRange = false;
-  if (!CheckMargins(60, 100, pulseRate)) {
+  if (!CheckMargins(PULSE_RATE_LOWER_LIMIT, PULSE_RATE_UPPER_LIMIT, pulseRate)) {
     cout << "Pulse Rate is out of range!\n";
     ShowWarning();
     isPulseRateOutOfRange = true;
@@ -44,7 +44,7 @@ bool isPulseRateOutOfRange(float pulseRate) {
 
 bool isSpo2OutOfRange(float spo2) {
   bool isSpo2OutOfRange = false;
-  if (!CheckMargins(90, 100, spo2)) {
+  if (!CheckMargins(SPO2_LOWER_LIMIT, SPO2_UPPER_LIMIT, spo2)) {
     cout << "Oxygen Saturation out of range!\n";
     ShowWarning();
     isSpo2OutOfRange = true;
--- test-monitor.cpp
+++ test-monitor.cpp
@@ -3,32 +3,47 @@
 #include "gtest/gtest.h"
 
 TEST(Monitor, EverythingIsOk) {
-  ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+  ASSERT_TRUE(vitalsOk(TEMPERATURE_LOWER_LIMIT + 1.0, PULSE_RATE_LOWER_LIMIT + 1.0, SPO2_LOWER_LIMIT + 1.0));
 }
 TEST(Monitor, OnlyLowTemperature) {
-  ASSERT_FALSE(vitalsOk(90, 75, 100));
+  ASSERT_FALSE(vitalsOk(TEMPERATURE_LOWER_LIMIT - 0.1, PULSE_RATE_LOWER_LIMIT + 1.0, SPO2_LOWER_LIMIT + 1.0));
 }
 TEST(Monitor, OnlyHighTemperature) {
-  ASSERT_FALSE(vitalsOk(105, 75, 100));
+  ASSERT_FALSE(vitalsOk(TEMPERATURE_UPPER_LIMIT + 0.1, PULSE_RATE_LOWER_LIMIT + 1.0, SPO2_LOWER_LIMIT + 1.0));
 }
 TEST(Monitor, OnlyLowPulseRate) {
-  ASSERT_FALSE(vitalsOk(99, 50, 100));
+  ASSERT_FALSE(vitalsOk(TEMPERATURE_LOWER_LIMIT + 1.0, PULSE_RATE_LOWER_LIMIT - 0.1, SPO2_LOWER_LIMIT + 1.0));
 }
 TEST(Monitor, OnlyHighPulseRate) {
-  ASSERT_FALSE(vitalsOk(99, 102, 100));
+  ASSERT_FALSE(vitalsOk(TEMPERATURE_LOWER_LIMIT + 1.0, PULSE_RATE_UPPER_LIMIT + 0.1, SPO2_LOWER_LIMIT + 1.0));
 }
-TEST(Monitor, OnlySpo2IsOffRange) {
-  ASSERT_FALSE(vitalsOk(99, 75, 70));
+TEST(Monitor, OnlyLowSpo2) {
+  ASSERT_FALSE(vitalsOk(TEMPERATURE_LOWER_LIMIT + 1.0, PULSE_RATE_LOWER_LIMIT + 1.0, SPO2_LOWER_LIMIT - 0.1));
 }
-TEST(Monitor, WhenTemperatureAndPulseRateAreOffRange) {
-  ASSERT_FALSE(vitalsOk(105, 102, 100));
+TEST(Monitor, OnlyHighSpo2) {
+  ASSERT_FALSE(vitalsOk(TEMPERATURE_LOWER_LIMIT + 1.0, PULSE_RATE_LOWER_LIMIT + 1.0, SPO2_UPPER_LIMIT + 0.1));
 }
-TEST(Monitor, WhenTemperatureAndSpo2AreOffRange) {
-  ASSERT_FALSE(vitalsOk(105, 75, 70));
+TEST(Monitor, WhenLowTemperatureAndLowPulseRate) {
+  ASSERT_FALSE(vitalsOk(TEMPERATURE_LOWER_LIMIT - 0.1, PULSE_RATE_LOWER_LIMIT - 0.1, SPO2_LOWER_LIMIT + 1.0));
 }
-TEST(Monitor, WhenPulseRateAndSpo2AreOffRange) {
-  ASSERT_FALSE(vitalsOk(99, 102, 70));
+TEST(Monitor, WhenHighTemperatureAndHighPulseRate) {
+  ASSERT_FALSE(vitalsOk(TEMPERATURE_UPPER_LIMIT + 0.1, PULSE_RATE_UPPER_LIMIT + 0.1, SPO2_LOWER_LIMIT + 1.0));
 }
-TEST(Monitor, WhenAllAreInBorderLine) {
-  ASSERT_FALSE(vitalsOk(102, 100, 90));
+TEST(Monitor, WhenLowTemperatureAndLowSpo2) {
+  ASSERT_FALSE(vitalsOk(TEMPERATURE_LOWER_LIMIT - 0.1, PULSE_RATE_LOWER_LIMIT + 1.0, SPO2_LOWER_LIMIT - 0.1));
+}
+TEST(Monitor, WhenHighTemperatureAndHighSpo2) {
+  ASSERT_FALSE(vitalsOk(TEMPERATURE_UPPER_LIMIT + 0.1, PULSE_RATE_LOWER_LIMIT + 1.0, SPO2_UPPER_LIMIT + 0.1));
+}
+TEST(Monitor, WhenLowPulseRateAndLowSpo2) {
+  ASSERT_FALSE(vitalsOk(TEMPERATURE_LOWER_LIMIT + 1.0, PULSE_RATE_LOWER_LIMIT - 0.1, SPO2_LOWER_LIMIT - 0.1));
+}
+TEST(Monitor, WhenHighPulseRateAndHighSpo2) {
+  ASSERT_FALSE(vitalsOk(TEMPERATURE_LOWER_LIMIT + 1.0, PULSE_RATE_UPPER_LIMIT + 0.1, SPO2_UPPER_LIMIT + 0.1));
+}
+TEST(Monitor, WhenAllAreInLowerBorderLine) {
+  ASSERT_TRUE(vitalsOk(TEMPERATURE_LOWER_LIMIT, PULSE_RATE_LOWER_LIMIT, SPO2_LOWER_LIMIT));
+}
+TEST(Monitor, WhenAllAreInUpperBorderLine) {
+  ASSERT_TRUE(vitalsOk(TEMPERATURE_UPPER_LIMIT, PULSE_RATE_UPPER_LIMIT, SPO2_UPPER_LIMIT));
 }
